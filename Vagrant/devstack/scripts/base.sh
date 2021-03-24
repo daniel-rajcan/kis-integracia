1225aade2c60327d8015d65490e685fc76e90063
Daniel Rajcan <drajcan@ohpen.com>
2021-03-24 01:44:54 +0100

    [Vagrant] Update OS AMI and fix reboot issue

--- Vagrant/devstack/scripts/base.sh
+++ Vagrant/devstack/scripts/base.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 apt-get -y update && apt-get -y upgrade
-apt-get -y install vim nano htop git tcpdump
+apt-get -y install vim nano htop git tcpdump net-utils bridge-utils
--- Vagrant/devstack/scripts/install-devstack.sh
+++ Vagrant/devstack/scripts/install-devstack.sh
@@ -1,29 +1,37 @@
 #!/bin/bash
 
+RELEASE="victoria"
+
 PASSWORD="$(echo $1 | grep -Po '^--\K.+')"
 HOST_IP="10.0.0.25"
+FLOATING_RANGE="172.25.0.0/24"
+GATEWAY_IP=$(sed -E "s/[[:digit:]]+(\/24)/1\1/" <<< $FLOATING_RANGE)
 
-cd /tmp
+cd /home/vagrant
 git clone https://opendev.org/openstack/devstack
 cd devstack
 
-git checkout stable/victoria
+git checkout stable/${RELEASE}
 
 cat << EOF > local.conf
 [[local|localrc]]
+HOST_IP=$HOST_IP
+SERVICE_HOST=$HOST_IP
 ADMIN_PASSWORD=$PASSWORD
 DATABASE_PASSWORD=$PASSWORD
 RABBIT_PASSWORD=$PASSWORD
 SERVICE_PASSWORD=$PASSWORD
-HOST_IP=$HOST_IP
+FLOATING_RANGE=$FLOATING_RANGE
+
+PIP_UPGRADE=True
 
 Q_AGENT=linuxbridge
 LB_PHYSICAL_INTERFACE=enp0s3
 PUBLIC_PHYSICAL_NETWORK=default
 LB_INTERFACE_MAPPINGS=default:enp0s3
 
-enable_plugin heat https://opendev.org/openstack/heat stable/victoria
-enable_plugin heat-dashboard https://opendev.org/openstack/heat-dashboard stable/victoria
+enable_plugin heat https://opendev.org/openstack/heat stable/${RELEASE}
+enable_plugin heat-dashboard https://opendev.org/openstack/heat-dashboard stable/${RELEASE}
 EOF
 
 ./stack.sh
@@ -43,3 +51,33 @@ source /home/vagrant/admin-openrc
 
 openstack user set --project demo demo
 openstack user set --project admin admin
+
+
+### Required to be able to restart vagrant instance without disruption of OpenStack functionality
+cat << EOF > /usr/local/bin/fix-bridge.sh
+brctl addbr br-ex
+ifconfig br-ex up
+ifconfig br-ex ${GATEWAY_IP}
+
+iptables -t nat -I POSTROUTING 1 -o enp0s3 -j MASQUERADE
+
+systemctl restart devstack@*
+EOF
+
+chmod 744 /usr/local/bin/fix-bridge.sh
+
+cat << EOF > /etc/systemd/system/vagrant@fix-bridge.service
+[Unit]
+After=sshd.service
+
+[Service]
+ExecStart=/bin/bash /usr/local/bin/fix-bridge.sh
+
+[Install]
+WantedBy=default.target
+EOF
+
+chmod 664 /etc/systemd/system/vagrant@fix-bridge.service
+
+systemctl daemon-reload
+systemctl enable vagrant@fix-bridge
